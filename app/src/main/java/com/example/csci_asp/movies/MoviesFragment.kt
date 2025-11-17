310fc7aed05019e0b58891faa80175af285b64c9
kushisantu <kushisantutennis@gmail.com>
2025-11-17 18:36:35 -0500

    dashboard home created

---
+++ app/src/main/java/com/example/csci_asp/movies/MoviesFragment.kt
@@ -0,0 +1,33 @@
+package com.example.csci_asp.movies
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.csci_asp.databinding.FragmentMoviesBinding
+
+class MoviesFragment : Fragment() {
+
+    private var _binding: FragmentMoviesBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMoviesBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
+
